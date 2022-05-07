da58c568f199a236cb9f520bf4d67d464624ce2f
Tom Harris <tomharris88@outlook.com>
2022-05-06 20:07:48 -0500

    added int method

--- src/LeetCode/DumpExtensions.cs
+++ src/LeetCode/DumpExtensions.cs
@@ -10,5 +10,17 @@ namespace LeetCode
             Console.Write(string.Join(" ", array.Select(x => x.ToString())));
             return array;
         }
+
+        public static string Dump(this string str)
+        {
+            Console.WriteLine(str);
+            return str;
+        }
+
+        public static int Dump(this int val)
+        {
+            Console.WriteLine(val);
+            return val;
+        }
     }
 }
